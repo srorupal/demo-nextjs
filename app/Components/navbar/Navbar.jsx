1f5654f1cb4f0bee6c1bcb6ce95604411ce52633
Divya <divya.orupalli@accionlabs.com>
2023-09-04 21:20:20 +0530

    NextJs Initial setup and configuration

---
+++ app/Components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import React from "react";
+
+const links = [
+    {
+        id: 1,
+        title: "Home",
+        url: "/"
+    },
+    {
+        id: 2,
+        title: "Portfolio",
+        url: "/portfolio"
+    },
+    {
+        id: 3,
+        title: "Blog",
+        url: "/blog"
+    },
+    {
+        id: 4,
+        title: "Blog",
+        url: "/blog"
+    },
+    {
+        id: 5,
+        title: "About",
+        url: "/about"
+    },
+    {
+        id: 5,
+        title: "Dashboard",
+        url: "/dashboard"
+    },
+    {
+        id: 6,
+        title: "Contact",
+        url: "/contact"
+    },
+];
+
+const Navbar = () => {
+    return (
+        <div>
+            <Link href="/home">Footer</Link>
+            <div>
+                {links.map((link)=>
+                <Link key={link.id} href={link.url}>{link.title}</Link>
+                )}
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
