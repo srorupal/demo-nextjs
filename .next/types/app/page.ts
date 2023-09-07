7fae8fb06c355755bb3d43c307ff4f4f1f1386e0
Divya <divya.orupalli@accionlabs.com>
2023-09-07 22:14:48 +0530

    Routes examples added using folder

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\demo-nextjs\app\page.tsx
+// File: D:\demo-nextjs\app\page.jsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
